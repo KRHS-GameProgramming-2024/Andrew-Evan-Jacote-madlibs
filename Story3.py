4fd1ee37ee9d96535524af9c743e3e7be79bc331
Evanhen466 <Henleye874@gmail.com>
2023-11-01 08:36:49 -0400

    11/1/23

--- Story3.py
+++ Story3.py
@@ -6,13 +6,13 @@ def Story3(debug = False):
     print("\n")
     friendName3 = getWord("Enter a name: ", debug)
     movie1 = getMovie("Enter a name of a Movie: ", debug)
-    food1 = getFood("Enter a name of a Food: ", debug)
+    food1 = getfood("Enter a name of a Food: ", debug)
     
     out = "\n"
     out += "One day me and my friend, " + friendName3
     out += " decided we wanted to go see , " + movie1
-    out += "when we got to the theatre we decided to get" + food1
-    out += "we sit down and leave halfway through " + movie1 + " because ot was bad"
+    out += " when we got to the theatre we decided to get" + food1
+    out += " we sit down and leave halfway through " + movie1 + " because it was bad"
     
     
     return out
