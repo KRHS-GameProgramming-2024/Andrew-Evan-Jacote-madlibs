abba50369f7e27d0b1a8520d98ee9bc8bf683646
Evanhen466 <Henleye874@gmail.com>
2023-11-03 08:15:24 -0400

    11

--- story4.py
+++ story4.py
@@ -1,1 +1,18 @@
 from Getters import *
+
+def Story4(debug = False):
+    if debug: print("Story4 Function")
+
+    print("\n")
+    friendName3 = getWord("Enter a name: ", debug)
+    badname1 = getbadname("Enter a name of a bad person: ", debug)
+    death1 = getdeath("Enter a name of a painful way to die: ", debug)
+    
+    out = "\n"
+    out += "One day me and my friend, " + friendName3
+    out += " decided we wanted to go see , " + movie1
+    out += " when we got to the theatre we decided to get " + food1
+    out += " we sit down and leave halfway through " + movie1 + " because it was bad"
+    
+    
+    return out
